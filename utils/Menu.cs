78daed2d23b70e8c965589dfd76fe0db416073f8
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-01-24 10:44:53 +0000

    add fills agen

--- utils/Menu.cs
+++ utils/Menu.cs
@@ -17,6 +17,7 @@ public class Menu(string[] options)
     /// <returns></returns>
     public int show()
     {
+        Console.Clear();
         bool isSelected = false;
         Console.WriteLine("Press arrow up and down to navigate. Select with enter");
         (int Left, int Top) = Console.GetCursorPosition();

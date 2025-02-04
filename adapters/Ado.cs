6c1472515e04d1202b75699e3b2de56322864bf7
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-02-04 07:55:50 +0100

    add comments

--- adapters/Ado.cs
+++ adapters/Ado.cs
@@ -6,6 +6,13 @@ public class Ado
 {
     public static string ConnectionString { get; set; } = "Data Source=db; Database=SkolaJosef; User id = sa; Password = P@ssw0rd; Trust Server Certificate=True;";
 
+    /// <summary>
+    /// Execute a query.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="map"></param>
+    /// <returns></returns>
     public static List<T> Query<T>(string query, Func<SqlDataReader, T> map)
     {
         var list = new List<T>();
@@ -23,14 +30,25 @@ public class Ado
         return list;
     }
 
+    /// <summary>
+    /// Execute a query.
+    /// </summary>
+    /// <param name="query"></param>
+    /// <param name="parameters"></param>
+    /// <returns>The last inserted id</returns>
     public static int Query(string query, SqlParameter[] parameters)
     {
         using var connection = new SqlConnection(ConnectionString);
         connection.Open();
 
-        using var command = new SqlCommand(query, connection);
+        using var command = new SqlCommand(query + "; SELECT SCOPE_IDENTITY()", connection);
         command.Parameters.AddRange(parameters);
+
+        // Execute the query and get the last inserted id
+        var result = command.ExecuteScalar();
+        var id = Convert.ToInt32(result);
+
         connection.Close();
-        return command.ExecuteNonQuery();
+        return id;
     }
 }

e728e57a487294941a6487d07d4eb305ab4e4d8f
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-02-05 10:33:16 +0000

    update primary key for role_grope table in migration

---
+++ Migrations/20250205103118_id.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DB_labb_3.Migrations
+{
+    /// <inheritdoc />
+    public partial class id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK__role_gro__3213E83FB6BE31D0",
+                table: "role_grope");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK__role_gro__3213E83FC465BE71",
+                table: "role_grope",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK__role_gro__3213E83FC465BE71",
+                table: "role_grope");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK__role_gro__3213E83FB6BE31D0",
+                table: "role_grope",
+                column: "id");
+        }
+    }
+}

e2f001e5ff891bc34ef1df4ca987a4bab1351a92
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-02-08 06:25:30 +0100

    Create GradeRepository.cs

---
+++ Repository/GradeRepository.cs
@@ -0,0 +1,22 @@
+using DB_labb_3.Interface;
+using DB_labb_3.Models;
+
+namespace DB_labb_3.Repository;
+
+public class GradeRepository : IRepository<Grade>
+{
+    public Grade Create(Grade entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Grade> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Grade> Get(string columnName, int id)
+    {
+        
+    }
+}
